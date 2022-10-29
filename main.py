76f37045eab7ea23182b2d97d56c4b451722967e
Samuel <samuelmalacarne.sm@gmail.com>
2022-10-29 18:37:38 +0200

    Addeds the possibility to move the cube with 'A' (or left arrow) and 'D' (or right arrow)

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from turtle import width
 import pygame, sys
 from pygame import Vector2
 import math
@@ -19,7 +18,7 @@ def dot(center, color = 'white'):
     pygame.draw.circle(screen, color, center, 1)
 
 def line(st_pos, end_pos, color = 'white'):
-    pygame.draw.line(screen, color, st_pos, end_pos, 2)
+    pygame.draw.line(screen, color, st_pos, end_pos, 3)
 
 def top_rot_line(D, d, angle):
     x = D * math.cos(math.radians(angle)) + (WIDTH/2)
@@ -43,7 +42,6 @@ class Cube():
     def render(self):
         top_p1 = top_rot_line(self.D, self.d, self.t + 0)
         bot_p1 = bot_rot_line(self.D, self.d, self.t + 0)
-        line(top_p1, bot_p1)
 
         top_p2 = top_rot_line(self.D, self.d, self.t + 90)
         bot_p2 = bot_rot_line(self.D, self.d, self.t + 90)
@@ -57,15 +55,16 @@ class Cube():
         bot_p4= bot_rot_line(self.D, self.d, self.t + 270)
         line(top_p4, bot_p4)
 
+        line(bot_p1, bot_p2)
+        line(bot_p2, bot_p3)
+        line(bot_p3, bot_p4)
+        line(top_p1, bot_p1, 'red')
+        line(bot_p4, bot_p1)
+
         line(top_p1, top_p2)
         line(top_p2, top_p3)
         line(top_p3, top_p4)
         line(top_p4, top_p1)
-
-        line(bot_p1, bot_p2)
-        line(bot_p2, bot_p3)
-        line(bot_p3, bot_p4)
-        line(bot_p4, bot_p1)
 
 class Triangle():
     def __init__(self, big_diamiter, small_diamiter, center):
@@ -82,7 +81,7 @@ class Triangle():
         bot_p3 = bot_rot_line(self.D, self.d, (self.t + 180))
         bot_p4 = bot_rot_line(self.D, self.d, (self.t + 270))
 
-        line(top_p, bot_p1)
+        line(top_p, bot_p1, 'red')
         line(top_p, bot_p2)
         line(top_p, bot_p3)
         line(top_p, bot_p4)
@@ -96,6 +95,8 @@ cube = Cube(200, 25, Vector2((WIDTH/2, HEIGHT/2)))
 triangle = Triangle(200, 25, Vector2((WIDTH/2, HEIGHT/2)))
 
 while True:
+    keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
@@ -104,12 +105,18 @@ while True:
 
         if event.type == SCREEN_UPDATE:
             # triangle.t += 0.1
-            cube.t += 0.1
+            cube.t += 0.01
 
 
     screen.fill((25, 25, 25))
     # triangle.render()
     cube.render()
+
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        cube.t += 0.9
+
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        cube.t -= 2
 
     pygame.display.flip()
     clock.tick(FPS)
